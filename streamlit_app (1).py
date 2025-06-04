d9cd6bde71af5b3b135aa5b0fc42f81d1e773967
TaNeekwa <taneekwa@outlook.com>
2025-06-04 05:34:47 -0400

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -107,7 +107,7 @@ top_col1, top_col2 = st.columns([1, 9])  # Layout split
 
 with top_col1:
     st.markdown("""
-    <div style="padding-top: 5px; text-align: left;">
+    <div style="padding-top: 5px; text-align: center;">
         <span style="font-weight: bold;"></span><br>
     </div>
     """, unsafe_allow_html=True)
