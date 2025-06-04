48901d08051cf4b5bf8a7923b63d393d423dd642
TaNeekwa <taneekwa@outlook.com>
2025-06-04 05:26:45 -0400

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -116,7 +116,7 @@ with top_col2:
   st.markdown("""
 <div style="width: 100vw; display: flex; flex-direction: column; align-items: center; justify-content: center; padding-top: 10px;">
     <img src="https://raw.githubusercontent.com/TaNeekwa/Flexgen-PG3x/main/FlexGen_Primary_Logo_-_Gradient.svg.png" 
-         alt="FlexGen Logo" width="300" style="margin-bottom: 10px;" />
+         alt="FlexGen Logo" width="100" style="margin-bottom: 10px;" />
     <h1 style="font-size: 42px; text-align: center; margin-top: 0;">
         Proposal Generator - FlexGen Edition
     </h1>
