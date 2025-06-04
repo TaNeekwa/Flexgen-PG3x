c85e9550fc85f63fc55b177f857abb6d5e72b5df
TaNeekwa <taneekwa@outlook.com>
2025-06-04 13:14:12 -0400

    Update streamlit_app (1).py

--- streamlit_app (1).py
+++ streamlit_app (1).py
@@ -15,7 +15,7 @@ if "authenticated" not in st.session_state:
 if not st.session_state.authenticated:
     # Logo
     st.markdown("""
-        <div style='display: flex; flex-direction: column; align-items: center; margin-top: 40px;'>
+        <div style='display: flex; flex-direction: column; align-items: center; margin-top: 30px;'>
             <img src='https://raw.githubusercontent.com/TaNeekwa/Flexgen-PG3x/main/FlexGen_Primary_Logo_-_Gradient.svg.png' width='300' style='margin-bottom: 40px;' />
         </div>
     """, unsafe_allow_html=True)
